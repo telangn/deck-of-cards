191da94b578737c75505fafb38e3cd861f0b15b6
Ninad Telang <telangn@gmail.com>
2021-01-17 21:44:12 -0800

    Added more tests

--- src/deck-of-cards-endpoint.spec.ts
+++ src/deck-of-cards-endpoint.spec.ts
@@ -6,28 +6,31 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 const url = 'https://deckofcardsapi.com';
 
-describe('Deck of Cards', () => {
+describe('Deck of Cards API', () => {
 
-    it('Should return a success', () => {
-        return chai.request(url).get('/api/deck/new').then(response => {
-            expect(response).to.be.status(200);
-            expect(response.body.remaining).to.equal(52);
+    var deckId: any;
+
+    it('Should create a deck with Jokers', () => {
+        return chai.request(url).get('/api/deck/new').query({ jokers_enabled: 'true' }).then(res => {
+            expect(res).to.be.status(200);
+            expect(res.body.remaining).to.equal(54);
+            deckId = res.body.deck_id;
         });
     });
 
-    it('Should be able to add Jokers', () => {
-        return chai.request(url).get('/api/deck/new').query({ jokers_enabled: 'true' }).then(response => {
-            expect(response.body.remaining).to.equal(54);
+    it('Should shuffle deck', () => {
+        return chai.request(url).get(`/api/deck/${deckId}/shuffle`).then(res => {
+            expect(res).to.be.status(200);
+            expect(res.body.shuffled).to.be.true;
         });
     });
-    
-    it('Should be able to draw two cards', async () => {
-        var deckId;
-        await chai.request(url).get('/api/deck/new').then(response => {
-            deckId = response.body.deck_id;
-        });
-        await chai.request(url).get(`/api/deck/${deckId}/draw/?count=2`).then(response => {
-            expect(response.body.remaining).to.equal(50);
+
+    it('Should draw two cards', () => {
+        return chai.request(url).get(`/api/deck/${deckId}/draw/?count=2`).then(res => {
+            expect(res).to.be.status(200);
+            expect(res.body.remaining).to.equal(52);
+            console.log('First Card Drawn: ' + res.body.cards[0].value + ' ' + res.body.cards[0].suit);
+            console.log('Second Card Drawn: ' + res.body.cards[1].value + ' ' + res.body.cards[1].suit);
         });
     });
 });
